e88b0963439bf7ce7ccc95a8b104a81d3ae973d2
jrjg <jendrik.jordan@web.de>
2015-11-27 17:12:32 +0100

    detail view buttons implementation
    awd

--- MyApplication2/app/src/main/java/com/example/simon/photojsl/DetailView.java
+++ MyApplication2/app/src/main/java/com/example/simon/photojsl/DetailView.java
@@ -1,17 +1,24 @@
 package com.example.simon.photojsl;
 
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.provider.MediaStore.Images;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ImageView;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 
 public class DetailView extends AppCompatActivity {
-
+    private File m_pic;
+    private ImageView m_iV;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,7 +27,7 @@ public class DetailView extends AppCompatActivity {
         String filename = intent.getStringExtra("Titel");
 
         //Context context = getApplicationContext();
-        File pic = new File(getApplicationContext().getExternalFilesDir(""), filename);
+        m_pic = new File(getApplicationContext().getExternalFilesDir(""), filename);
         //BitmapWorkerTask task = new BitmapWorkerTask((ImageView)findViewById(R.id.imageView_Detail));
         //task.execute(path);
         /*final BitmapFactory.Options options = new BitmapFactory.Options();
@@ -30,9 +37,9 @@ public class DetailView extends AppCompatActivity {
         options.inJustDecodeBounds = false;
         Bitmap b = BitmapFactory.decodeFile(path, options);
         //Bitmap thumbnail = BitmapFactory.decodeFile(pic.getAbsolutePath());*/
-        ImageView iV = (ImageView) findViewById(R.id.imageView_Detail);
-        if (pic != null) {
-            iV.setImageURI(Uri.fromFile(pic));
+        m_iV = (ImageView) findViewById(R.id.imageView_Detail);
+        if (m_pic != null) {
+            m_iV.setImageURI(Uri.fromFile(m_pic));
         }
 
 
@@ -40,22 +47,38 @@ public class DetailView extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main_menu, menu);
+        getMenuInflater().inflate(R.menu.detail_menu, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch(id){
+            case R.id.action_delete:
+                deleteFile(m_pic.getAbsolutePath());
+                break;
+            case R.id.action_send:
+                final Intent emailIntent = new Intent(android.content.Intent.ACTION_SEND);
+                emailIntent.setType("plain/text");
+                emailIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, m_pic.getName());
+                emailIntent.putExtra(android.content.Intent.EXTRA_TEXT, "powered by JLS Software");
+                emailIntent.setType("image/png");
+                Uri imageUri = Uri.parse(m_pic.getAbsolutePath());
+                emailIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                emailIntent.putExtra(Intent.EXTRA_STREAM, imageUri);
+                startActivity(Intent.createChooser(emailIntent, "Send mail"));
+                break;
+            case R.id.action_save:
+                try {
+                    Images.Media.insertImage(getContentResolver(), m_pic.getAbsolutePath(),m_pic.getName(), "JLS Picture");
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+                break;
         }
-
         return super.onOptionsItemSelected(item);
     }
 }
